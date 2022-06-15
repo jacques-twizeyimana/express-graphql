08dfaf46a2f813e1630e052a2582977bd5ad3ae4
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-06-15 23:49:44 +0200

    feat: create static database

---
+++ static/data.js
@@ -0,0 +1,81 @@
+const authors = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe1@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "johndoe2@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Jane Doe",
+    email: "johndoe3@gmail.com",
+  },
+];
+
+const books = [
+  {
+    id: 1,
+    name: "Book 1",
+    authorId: 1,
+  },
+  {
+    id: 2,
+    name: "Book 2",
+    authorId: 2,
+  },
+  {
+    id: 3,
+    name: "Book 3",
+    authorId: 3,
+  },
+  {
+    id: 4,
+    name: "Book 4",
+    authorId: 1,
+  },
+  {
+    id: 5,
+    name: "Book 1",
+    authorId: 1,
+  },
+  {
+    id: 6,
+    name: "Book 2",
+    authorId: 2,
+  },
+  {
+    id: 7,
+    name: "Book 3",
+    authorId: 3,
+  },
+  {
+    id: 8,
+    name: "Book 4",
+    authorId: 1,
+  },
+
+  {
+    id: 9,
+    name: "Book 2",
+    authorId: 2,
+  },
+  {
+    id: 10,
+    name: "Book 3",
+    authorId: 3,
+  },
+  {
+    id: 11,
+    name: "Book 4",
+    authorId: 1,
+  },
+];
+
+module.exports = {
+  authors,
+  books,
+};
